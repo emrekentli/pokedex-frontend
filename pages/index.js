8c8819e0fbf93dff71759abdd446cb7d73f499b9
Emre <emrekentlii@gmail.com>
2023-08-09 23:22:43 +0300

    pokemon continues

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import {Column} from 'primereact/column';
 import {DataTable} from 'primereact/datatable';
 import {Menu} from 'primereact/menu';
 import React, {useContext, useEffect, useRef, useState} from 'react';
-import {ProductService} from '../demo/service/ProductService';
 import {LayoutContext} from '../layout/context/layoutcontext';
 import Link from 'next/link';
 
@@ -31,7 +30,6 @@ const lineData = {
 };
 
 const Dashboard = () => {
-    const [products, setProducts] = useState(null);
     const menu1 = useRef(null);
     const menu2 = useRef(null);
     const [lineOptions, setLineOptions] = useState(null);
@@ -102,7 +100,6 @@ const Dashboard = () => {
     };
 
     useEffect(() => {
-        ProductService.getProductsSmall().then((data) => setProducts(data));
     }, []);
 
     useEffect(() => {
@@ -183,20 +180,6 @@ const Dashboard = () => {
             <div className="col-12 xl:col-6">
                 <div className="card">
                     <h5>Recent Sales</h5>
-                    <DataTable value={products} rows={5} paginator responsiveLayout="scroll">
-                        <Column header="Image" body={(data) => <img className="shadow-2" src={`/demo/images/product/${data.image}`} alt={data.image} width="50" />} />
-                        <Column field="name" header="Name" sortable style={{ width: '35%' }} />
-                        <Column field="price" header="Price" sortable style={{ width: '35%' }} body={(data) => formatCurrency(data.price)} />
-                        <Column
-                            header="View"
-                            style={{ width: '15%' }}
-                            body={() => (
-                                <>
-                                    <Button icon="pi pi-search" type="button" text />
-                                </>
-                            )}
-                        />
-                    </DataTable>
                 </div>
                 <div className="card">
                     <div className="flex justify-content-between align-items-center mb-5">
